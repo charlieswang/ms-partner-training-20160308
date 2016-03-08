6992e050925a5142625fc4fc4a2f4633145cbe73
GeorgeChao <whizzalan@gmail.com>
2016-03-08 08:13:23 +0800

    modify liveDemo.R

--- part3/liveDemo.R
+++ part3/liveDemo.R
@@ -1,12 +1,16 @@
 
 ## Memory Size Is Not Enough ##
 library(data.table)
-## ##
 temp <- fread("~/data/rre/AirOnTimeCSV/airOT198710.csv")
 temp %>% object.size %>% format( units = "MB")
 
-
-
-
+## News ##
 load(file = "~/data/MSPartnerDemo/vsv.RDA")
 load(file = "~/data/MSPartnerDemo/post.RDA")
+
+library(dplyr)
+
+VAV %>% group_by(v1, v2) %>% summarise(count = n_distinct()) %>% dplyr::arrange(desc(count)) %>% head(100)
+head(VAV)
+head(posts)
+
